79ab7b7ba1b48796ce770f3fc83ea5741d1e5aef
Shirlee Berman <shirlee.berman@gmail.com>
2014-01-21 17:47:35 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mcrdash::Application.config.secret_token = '966cd2fc282eeb6177ce59313e5e28e9c79d11d51fe7187a1145dfb31ba5cd763699fa043783c44664dee24c269ac94d647c9905d8ec64a28be8b3cd66cf7f86'
